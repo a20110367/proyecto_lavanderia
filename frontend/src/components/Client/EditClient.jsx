9f87aee63cfe4170865627906ffb3f8587505ac5
Saul Rodriguez Reynoso <a20110347@ceti.mx>
2023-09-25 15:24:03 -0600

    EditClient Unused Imports Deleted

--- frontend/src/components/Client/EditClient.jsx
+++ frontend/src/components/Client/EditClient.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import Axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
 import {
@@ -7,7 +7,6 @@ import {
   faInfoCircle,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import NavBar from "../../routes/Navbar";
 
 const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[?=!@#$%*]).{8,24}$/;

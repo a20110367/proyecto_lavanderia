eecf2c428cf8b10c6244c6ccd41731d3e38b5734
Saul <hebrobine@gmail.com>
2023-10-24 14:29:22 -0600

    first format for ticket and IMAGES URLs Added

--- frontend/src/images/images.jsx
+++ frontend/src/images/images.jsx
@@ -1,5 +1,6 @@
 const IMAGES = {
-    pdfIcon : new URL('./img/hu.jpg', import.meta.url).href
+    pdfIcon : new URL('./img/hu.jpg', import.meta.url).href,
+    caprelogo : new URL('./img/caprelogo.png', import.meta.url).href
 }
 
 export default IMAGES 

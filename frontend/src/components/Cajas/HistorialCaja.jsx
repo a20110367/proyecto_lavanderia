df9d3fe9b9bb71a4baa326229962e60f01bd1f63
Axel <a20110341@ceti.mx>
2023-10-26 03:10:35 -0600

    DatePicker arreglado, Puntoventa regresa a cliente

--- frontend/src/components/Cajas/HistorialCaja.jsx
+++ frontend/src/components/Cajas/HistorialCaja.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Modal, Button, DatePicker } from "antd";
 import jsPDF from "jspdf";
-import { format } from "date-fns";
 
 function HistorialCaja() {
   const [Cortes, setCortes] = useState([]);
@@ -15,24 +14,24 @@ function HistorialCaja() {
     const dummyCortes = [
       {
         id: 1,
-        fecha: "2023-09-20",
-        dineroFondo: 20000,
-        retirosTotales: 1200,
-        ingresosTotales: 20000,
-        ingresoEfectivo: 10000,
-        ingresoTarjeta: 10000,
+        fecha: "15/09/2023", // Mantén el formato dd/mm/yyyy aquí
+        dineroFondo: 18000,
+        retirosTotales: 600,
+        ingresosTotales: 16000,
+        ingresoEfectivo: 9000,
+        ingresoTarjeta: 7000,
         finalTotalCaja: 0,
-        usuario: "Usuario1",
+        usuario: "Usuario3",
         turno: "Matutino",
-        tipoServicio: "Autoservicio",
+        tipoServicio: "Planchado",
         // Añade los campos de ingresos por servicio
-        ingresoAutoservicio: 10000,
-        ingresoLavadoEncargo: 16000,
-        ingresoPlanchado: 15000,
+        ingresoAutoservicio: 3000,
+        ingresoLavadoEncargo: 4000,
+        ingresoPlanchado: 16000,
       },
       {
         id: 2,
-        fecha: "2023-09-18",
+        fecha: "18/09/2023", // Mantén el formato dd/mm/yyyy aquí
         dineroFondo: 15000,
         retirosTotales: 300,
         ingresosTotales: 15000,
@@ -49,20 +48,20 @@ function HistorialCaja() {
       },
       {
         id: 3,
-        fecha: "2023-09-15",
-        dineroFondo: 18000,
-        retirosTotales: 600,
-        ingresosTotales: 16000,
-        ingresoEfectivo: 9000,
-        ingresoTarjeta: 7000,
+        fecha: "20/09/2023", // Mantén el formato dd/mm/yyyy aquí
+        dineroFondo: 20000,
+        retirosTotales: 1200,
+        ingresosTotales: 20000,
+        ingresoEfectivo: 10000,
+        ingresoTarjeta: 10000,
         finalTotalCaja: 0,
-        usuario: "Usuario3",
+        usuario: "Usuario1",
         turno: "Matutino",
-        tipoServicio: "Planchado",
+        tipoServicio: "Autoservicio",
         // Añade los campos de ingresos por servicio
-        ingresoAutoservicio: 3000,
-        ingresoLavadoEncargo: 4000,
-        ingresoPlanchado: 16000,
+        ingresoAutoservicio: 10000,
+        ingresoLavadoEncargo: 16000,
+        ingresoPlanchado: 15000,
       },
     ];
     const cortesConFinalTotalCaja = dummyCortes.map((corte) => ({
@@ -101,41 +100,21 @@ function HistorialCaja() {
       doc.text(`ID: ${selectedCorte.id}`, 10, 20);
       doc.text(`Usuario: ${selectedCorte.usuario}`, 10, 30);
       doc.text(`Turno: ${selectedCorte.turno}`, 10, 40);
-      doc.text(
-        `Fecha: ${format(new Date(selectedCorte.fecha), "dd/MM/yyyy")}`,
-        10,
-        50
-      );
+      doc.text(`Fecha: ${selectedCorte.fecha}`, 10, 50);
       doc.text(`Dinero en Fondo: $${selectedCorte.dineroFondo}`, 10, 60);
 
       // Separación
       doc.text(`Detalles de Ingresos por Servicio:`, 10, 80);
       doc.text(`Autoservicio: $${selectedCorte.ingresoAutoservicio}`, 10, 90);
-      doc.text(
-        `Lavado por Encargo: $${selectedCorte.ingresoLavadoEncargo}`,
-        10,
-        100
-      );
+      doc.text(`Lavado por Encargo: $${selectedCorte.ingresoLavadoEncargo}`, 10, 100);
       doc.text(`Planchado: $${selectedCorte.ingresoPlanchado}`, 10, 110);
-      doc.text(
-        `Total (Suma de los Servicios): $${selectedCorte.ingresoTotalServicios}`,
-        10,
-        120
-      );
-      doc.text(
-        `Ingreso en Efectivo: $${selectedCorte.ingresoEfectivo}`,
-        10,
-        130
-      );
+      doc.text(`Total (Suma de los Servicios): $${selectedCorte.ingresoTotalServicios}`, 10, 120);
+      doc.text(`Ingreso en Efectivo: $${selectedCorte.ingresoEfectivo}`, 10, 130);
 
       // Separación
       doc.text(`Ingreso en Tarjeta: $${selectedCorte.ingresoTarjeta}`, 10, 150);
       doc.text(`Retiros Totales: $${selectedCorte.retirosTotales}`, 10, 160);
-      doc.text(
-        `Final Total en Caja: $${selectedCorte.finalTotalCaja}`,
-        10,
-        170
-      );
+      doc.text(`Final Total en Caja: $${selectedCorte.finalTotalCaja}`, 10, 170);
 
       doc.save("detalle_corte.pdf");
     }
@@ -149,8 +128,8 @@ function HistorialCaja() {
         return corteDate >= startDate && corteDate <= endDate;
       });
 
-      const startDateStr = format(startDate, "yyyy-MM-dd");
-      const endDateStr = format(endDate, "yyyy-MM-dd");
+      const startDateStr = startDate.toLocaleDateString("en-GB");
+      const endDateStr = endDate.toLocaleDateString("en-GB");
 
       const startCorte = Cortes.find((corte) => corte.fecha === startDateStr);
       const endCorte = Cortes.find((corte) => corte.fecha === endDateStr);
@@ -193,7 +172,7 @@ function HistorialCaja() {
                     }
                   }}
                   value={dateRange}
-                  format="DD/MM/YYYY" // Formato de fecha dd/mm/yyyy
+                  format="DD/MM/YYYY"
                   className="border-2 rounded-md py-2  pl-10  border-black mt-2"
                 />
                 <button
@@ -209,26 +188,24 @@ function HistorialCaja() {
             <table className="w-full text-sm text-left text-gray-500 ">
               <thead className="text-xs text-gray-700 uppercase bg-gray-200">
                 <tr>
-                  <th >ID</th>
+                  <th>ID</th>
                   <th>FECHA</th>
                   <th>DINERO EN FONDO</th>
-                  <th >INGRESO EN EFECTIVO</th>
-                  <th >INGRESO EN TARJETA</th>
+                  <th>INGRESO EN EFECTIVO</th>
+                  <th>INGRESO EN TARJETA</th>
                   <th>INGRESOS TOTALES</th>
-                  <th >RETIROS TOTALES</th>
-                  <th >FINAL TOTAL CAJA</th>
-                  <th >USUARIO</th>
-                  <th >TURNO</th>
-                  <th ></th>
+                  <th>RETIROS TOTALES</th>
+                  <th>FINAL TOTAL CAJA</th>
+                  <th>USUARIO</th>
+                  <th>TURNO</th>
+                  <th></th>
                 </tr>
               </thead>
               <tbody>
                 {filteredCortes.map((corte) => (
                   <tr className="bg-white border-b" key={corte.id}>
                     <td className="py-3 px-1 text-center">{corte.id}</td>
-                    <td className="py-3 px-6">
-                      {format(new Date(corte.fecha), "dd/MM/yyyy")}
-                    </td>
+                    <td className="py-3 px-6">{corte.fecha}</td>
                     <td className="py-3 px-6">${corte.dineroFondo}</td>
                     <td className="py-3 px-6">${corte.ingresoEfectivo}</td>
                     <td className="py-3 px-6">${corte.ingresoTarjeta}</td>
@@ -293,7 +270,7 @@ function HistorialCaja() {
                   </p>
                   <p className="text-lg">
                     <span className="font-bold">Fecha:</span>{" "}
-                    {format(new Date(selectedCorte.fecha), "dd/MM/yyyy")}
+                    {selectedCorte.fecha}
                   </p>
                   <p className="text-lg">
                     <span className="font-bold">Dinero en Fondo:</span> $

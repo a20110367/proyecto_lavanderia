9a247e980dd4b4c07630f62a2ec0423b4d4966f3
Saul <hebrobine@gmail.com>
2023-10-31 21:58:33 -0600

    Peditos

--- frontend/src/components/Service/ServicesLavanderia.jsx
+++ frontend/src/components/Service/ServicesLavanderia.jsx
@@ -97,11 +97,9 @@ function ServicesLavanderia() {
                 <th>Descripción</th>
                 <th>Categoria</th>
                 <th>Precio</th>
-                <th>Tiempo</th>
+                {/* <th>Tiempo</th>
                 <th>Peso</th>
-                <th>Piezas</th>
-                <th>Fecha de Creación</th>
-                <th>Fecha de Actualización</th>
+                <th>Piezas</th> */}
                 <th>Opciones</th>
               </tr>
             </thead>
@@ -115,16 +113,15 @@ function ServicesLavanderia() {
                   <tr key={service.id_service}>
                     <td>{index + 1}</td>
                     <td>{service.description}</td>
+                    <td>{service.Category.categoryDescription}</td>
                     <td>${service.price}</td>
-                    <td>{service.time} minutos</td>
+                    {/* <td>{service.time} minutos</td>
                     <td>
                       {service.weight} {service.weight ? "kg" : ""}
                     </td>
                     <td>
                       {service.pieces} {service.pieces ? "pz" : ""}
-                    </td>
-                    <td>{service.created}</td>
-                    <td>{service.updatedAT}</td>
+                    </td> */}
                     <td>
                       <button
                         onClick={() =>

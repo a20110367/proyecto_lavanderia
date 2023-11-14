94ab9a1cb0b6457fc1120d04def224934804b6db
a20110367 <a20110367@ceti.mx>
2023-11-14 11:07:02 -0600

    Caja chica update

--- backend/controllers/PettyCashController.js
+++ backend/controllers/PettyCashController.js
@@ -48,9 +48,28 @@ export const getPettyCashBalance = async (req, res) =>{
 
 export const createDepositPettyCash = async (req, res) =>{
     try {
+        
+        const lastPettyCash = await prisma.pettyCash.aggregate({
+            _max:{
+                id_movement:true,
+            }             
+        });
+
+        const balance = await prisma.pettyCash.get({
+            where:{
+                id_movement:lastPettyCash._max,
+            },
+            select:{
+                balance:true,
+            }
+        });
+
+        req.body.balance = balance + req.body.balance;
+
         const response = await prisma.pettyCash.create({
             data:req.body                 
         });
+
         res.status(200).json(response);
     }catch(e){
         res.status(500).json({msg:e.message});
@@ -59,6 +78,25 @@ export const createDepositPettyCash = async (req, res) =>{
 
 export const createWithdrawalPettyCash = async (req, res) =>{
     try {
+        
+        const lastPettyCash = await prisma.pettyCash.aggregate({
+            _max:{
+                id_movement:true,
+            }             
+        });
+
+        const balance = await prisma.pettyCash.get({
+            where:{
+                id_movement:lastPettyCash._max,
+            },
+            select:{
+                balance:true,
+            }
+        });
+
+        req.body.balance = balance - req.body.balance;
+
+        
         const response = await prisma.pettyCash.create({
             data:req.body                 
         });

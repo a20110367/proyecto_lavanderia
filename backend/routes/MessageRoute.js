69ce83f54a4fa5d5299353345c59678f08f676aa
Saul <hebrobine@gmail.com>
2023-11-15 17:20:45 -0600

    Notify all clients

--- backend/routes/MessageRoute.js
+++ backend/routes/MessageRoute.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import {
     sendMessage,
-    n2word
+    n2word,
+    notifyAll
 } from "../controllers/MessageController.js";
 
 const router = express.Router();
 
 router.post('/sendMessage', sendMessage);
 router.post('/numberToWord', n2word)
+router.post('/notifyAll', notifyAll)
 
 export default router;

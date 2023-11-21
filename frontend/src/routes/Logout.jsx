1504e2ca08281f54f804eda4312c8939c8abfe05
Saul <hebrobine@gmail.com>
2023-11-21 14:55:01 -0600

    Logout modified

--- frontend/src/routes/Logout.jsx
+++ frontend/src/routes/Logout.jsx
@@ -1,29 +1,36 @@
 import { useAuth } from '../hooks/auth/auth';
 import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from 'react';
 import Swal from 'sweetalert2'
+import api from '../api/api'
+import { useEffect } from 'react';
 
 function Logout() {
 
     const navigate = useNavigate();
-    const [cashCut, setCashCut] = useState()
     const { logout } = useAuth();
 
     useEffect(() => {
-        setCashCut(localStorage.getItem('lastCashCut'))
+        validate()
     }, []);
 
-    if (cashCut) {
-        logout()
-        localStorage.removeItem('lastCashCut')
-    } else {
-        Swal.fire({
-            icon: "warning",
-            title: "No se ha realizado el Corte de Caja",
-            text: 'Da click en Corte de Caja.',
-            confirmButtonColor: '#034078'
-        });
-        navigate('/corteCaja')
+    const validate = async () => {
+        try {
+            const res = await api.get("/cashCutStatus")
+            if (res.data.cashCutStatus === 'closed') {
+                logout()
+            } else if (res.data.cashCutStatus === 'open') {
+                Swal.fire({
+                    icon: "warning",
+                    title: "No se ha realizado el Corte de Caja",
+                    text: 'Da click en Corte de Caja.',
+                    confirmButtonColor: '#034078'
+                });
+                navigate('/corteCaja')
+            }
+        } catch (err) {
+            console.log(err)
+            console.error('No entiendo como sucedio esto')
+        }
     }
 }
 

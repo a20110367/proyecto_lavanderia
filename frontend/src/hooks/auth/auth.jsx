07b246a1781744ab933f11a40835f63624cccb92
Saul <hebrobine@gmail.com>
2023-11-29 20:19:43 -0600

    Auth y SelfService

--- frontend/src/hooks/auth/auth.jsx
+++ frontend/src/hooks/auth/auth.jsx
@@ -40,13 +40,14 @@ export const UserProvider = ({ children }) => {
             const res = await api.post("/ironControl", {
                 piecesToday: 0,
             });
-            console.log(res)
-            await api.patch(`/diaryIronControl/${res.data.id_ironControl}`);            
-            localStorage.setItem('lastIronControl', res.data.id_ironControl)
+            const resDiary = await api.patch(`/diaryIronControl/${res.data.id_ironControl}`); 
+            localStorage.setItem('numberOfPieces', resDiary.data.piecesToday)           
+            localStorage.setItem('lastIronControl', resDiary.data.id_ironControl)
         } else if(diff === 0){
             // console.log('mismo dia')
-            await api.patch(`/diaryIronControl/${lastIronControl.data[0].id_ironControl}`);
-            localStorage.setItem('lastIronControl', lastIronControl.data[0].id_ironControl)
+            const res = await api.patch(`/diaryIronControl/${lastIronControl.data[0].id_ironControl}`);    
+            localStorage.setItem('numberOfPieces', res.data.piecesToday)
+            localStorage.setItem('lastIronControl', res.data.id_ironControl)
         }else{
             localStorage.setItem('lastIronControl', lastIronControl.data[0].id_ironControl)
         }

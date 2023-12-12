cc9f32c752d5a73f79d38516a703121b6e0b4425
Axel <a20110341@ceti.mx>
2023-12-12 16:02:55 -0600

    Autoservicio fixed

--- frontend/src/components/PuntoVenta/PuntoVenta.jsx
+++ frontend/src/components/PuntoVenta/PuntoVenta.jsx
@@ -698,7 +698,7 @@ export default function PuntoVenta() {
                       key="submit"
                       className="mr-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                       onClick={handleSaveAndGenerateTicket}
-                      disabled={!isDeliveryDateSelected || isSaved}
+                      disabled={serviceType === "autoservicio" ? false : !isDeliveryDateSelected || isSaved}
                     >
                       Guardar
                     </button>,

6bdd9d882efdc0fd98a397a6d77786870cab8066
Saul <hebrobine@gmail.com>
2023-12-22 20:42:01 -0600

    Tasking node-thermal-printer

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,7 @@ import MessageRoute from './routes/MessageRoute.js'
 import IronControlRoute from './routes/IronControlRoute.js'
 import ServiceDryclean from './routes/ServiceDrycleanRoute.js'
 import OtherService from './routes/ServiceOtherServiceRoute.js'
-// import Ticket from './routes/TicketRoute.js'
+import Ticket from './routes/TicketRoute.js'
 
 dotenv.config({ path: '.env' });
 const app = express();
@@ -62,7 +62,7 @@ app.use(MessageRoute)
 app.use(IronControlRoute)
 app.use(ServiceDryclean)
 app.use(OtherService)
-// app.use(Ticket)
+app.use(Ticket)
 
 app.listen(process.env.APP_PORT, (req, res) => {
     console.log('SERVER RUNNING IN http://localhost:5000');
--- backend/controllers/TicketController.js
+++ backend/controllers/TicketController.js
@@ -1,80 +1,80 @@
 import { ThermalPrinter, PrinterTypes, CharacterSet, BreakLine } from 'node-thermal-printer';
 
-console.log(process.env.INTERFACE)
+console.log('PRINTER WORKING IN ' + process.env.INTERFACE)
 
 let printer = new ThermalPrinter({
     type: PrinterTypes.EPSON,
-    interface: 'printer:EPSON_TM-T20II',
+    interface: process.env.INTERFACE,
     characterSet: CharacterSet.PC852_LATIN2,
     removeSpecialCharacters: false,
     lineCharacter: "=",
-    breakLine: BreakLine.WORD, 
-    driver: ''
+    breakLine: BreakLine.WORD,
 });
+
+const execute = async () => {
+    let execute = printer.execute()
+}
 
 export const generateTicket = async () => {
 
-    console.log('starting')
-    console.log(printer)
-    let isConnected = await printer.isPrinterConnected();       // Check if printer is connected, return bool of status
-    // let execute = await printer.execute();                      // Executes all the commands. Returns success or throws error
-    // let raw = await printer.raw(Buffer.from("Hello world"));    // Print instantly. Returns success or throws error
-    console.log(isConnected)
-    if (isConnected) {
-        printer.print("Hello World");                               // Append text
+    try {
+        // printer.print("Hello World");                               // Append text
         printer.println("Hello World");                             // Append text with new line
-        printer.openCashDrawer();                                   // Kick the cash drawer
-        printer.cut();                                              // Cuts the paper (if printer only supports one mode use this)
-        printer.partialCut();                                       // Cuts the paper leaving a small bridge in middle (if printer supports multiple cut modes)
-        printer.beep();                                             // Sound internal beeper/buzzer (if available)
-        printer.upsideDown(true);                                   // Content is printed upside down (rotated 180 degrees)
-        printer.setCharacterSet(CharacterSet.PC852_LATIN2);         // Set character set - default set on init
-
-        printer.bold(true);                                         // Set text bold
-        printer.invert(true);                                       // Background/text color inversion
-        printer.underline(true);                                    // Underline text (1 dot thickness)
-        printer.underlineThick(true);                               // Underline text with thick line (2 dot thickness)
+        printer.println('PROBANDO LINE')
+        // printer.bold(true);                                         // Set text bold
+        // printer.invert(true);                                       // Background/text color inversion
+        // printer.underline(true);                                    // Underline text (1 dot thickness)
+        // printer.underlineThick(true);                               // Underline text with thick line (2 dot thickness)
         printer.drawLine();                                         // Draws a line
-        printer.newLine();                                          // Inserts break line
+        // printer.newLine();                                          // Inserts break line 
 
         printer.alignCenter();                                      // Align text to center
         printer.alignLeft();                                        // Align text to left
         printer.alignRight();                                       // Align text to right
 
-        printer.setTypeFontA();                                     // Set font type to A (default)
-        printer.setTypeFontB();                                     // Set font type to B
+        // printer.setTypeFontA();                                     // Set font type to A (default)
+        // printer.setTypeFontB();                                     // Set font type to B
 
-        printer.setTextNormal();                                    // Set text to normal
-        printer.setTextDoubleHeight();                              // Set text to double height
-        printer.setTextDoubleWidth();                               // Set text to double width
-        printer.setTextQuadArea();                                  // Set text to quad area
-        printer.setTextSize(7, 7);                                   // Set text height (0-7) and width (0-7)
+        // printer.setTextNormal();                                    // Set text to normal
+        // printer.setTextDoubleHeight();                              // Set text to double height
+        // printer.setTextDoubleWidth();                               // Set text to double width
+        // printer.setTextQuadArea();                                  // Set text to quad area
+        // printer.setTextSize(7, 7);                                   // Set text height (0-7) and width (0-7)
 
         printer.leftRight("Left", "Right");                         // Prints text left and right
         printer.table(["One", "Two", "Three"]);                     // Prints table equally
-        printer.tableCustom([                                       // Prints table with custom settings (text, align, width, cols, bold)
-            { text: "Left", align: "LEFT", width: 0.5 },
-            { text: "Center", align: "CENTER", width: 0.25, bold: true },
-            { text: "Right", align: "RIGHT", cols: 8 }
-        ]);
+        // printer.tableCustom([                                       // Prints table with custom settings (text, align, width, cols, bold)
+        //     { text: "Left", align: "LEFT", width: 0.5 },
+        //     { text: "Center", align: "CENTER", width: 0.25, bold: true },
+        //     { text: "Right", align: "RIGHT", cols: 8 }
+        // ]);
 
-        printer.code128("Code128");                                 // Print code128 bar code
-        printer.printQR("QR CODE");                                 // Print QR code
-        await printer.printImage('./assets/olaii-logo-black.png');  // Print PNG image
+        // printer.code128("Code128");                                 // Print code128 bar code
+        // printer.printQR("QR CODE");                                 // Print QR code
+        // await printer.printImage('./assets/olaii-logo-black.png');  // Print PNG image
 
-        printer.clear();                                              // Clears printText value
-        printer.getText();                                            // Returns printer buffer string value
-        printer.getBuffer();                                          // Returns printer buffer
-        printer.setBuffer(newBuffer);                                 // Set the printer buffer to a copy of newBuffer
-        printer.getWidth();
+        // printer.clear();                                              // Clears printText value
+        // printer.getText();                                            // Returns printer buffer string value
+        // printer.getBuffer();                                          // Returns printer buffer
+        // printer.setBuffer(newBuffer);                                 // Set the printer buffer to a copy of newBuffer
+        // printer.getWidth();
+
+        printer.printQR("Hola, Soy Homelo Pino", {
+            cellSize: 6,             // 1 - 8
+            correction: 'M',         // L(7%), M(15%), Q(25%), H(30%)
+            model: 2                 // 1 - Model 1
+                                     // 2 - Model 2 (standard)
+                                     // 3 - Micro QR
+        });
+
+        // for(let i = 0 ; i < 5 ; i++){
+        //     printer.print("SACAME LA VERGA LUPE " + process.env.INTERFACE);
+        // }
+        printer.cut();                                              // Cuts the paper (if printer only supports one mode use this)
+        execute()
+
+        console.log("Print done!");
+    } catch (error) {
+        console.error("Print failed:", error);
     }
-    printer.print("Hello World");
-
-    printer.execute(function(err){
-        if(err){
-            console.error('Something Happens', err)
-        }else{
-            console.log('Work Done')
-        }
-    })
 }

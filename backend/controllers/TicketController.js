eb75525e2e10c15b1516acf95ade91f9c94e97fe
Saul <hebrobine@gmail.com>
2023-12-24 16:07:54 -0600

    Ticket Update

--- backend/controllers/TicketController.js
+++ backend/controllers/TicketController.js
@@ -13,22 +13,94 @@ let printer = new ThermalPrinter({
 
 const execute = async () => {
     let execute = printer.execute()
+    console.log("Print done!");
 }
 
 export const generateTicket = async () => {
 
     try {
-        printer.println('PROBANDO LINE')
-        // printer.cut()
-        await printer.printImage('./controllers/utils/img/caprelogoThermalPrinterGray.png');
+        // LOGO DEL NEGOCIO
+        await printer.printImage('./controllers/utils/img/caprelogoThermalPrinterGrayINFO.png');
+
+        printer.drawLine();
+        printer.setTypeFontB();
+        
+        printer.setTextDoubleHeight();
+        printer.tableCustom([                                       // Prints table with custom settings (text, align, width, cols, bold)
+            { text: "Folio: 12+1", align: "LEFT", bold: true},
+            { text: "PAGADO", align: "RIGHT" }
+        ]);
+
+        printer.setTextNormal();
+        printer.tableCustom([                                       // Prints table with custom settings (text, align, width, cols, bold)
+            { text: "CATEGORIA: ENCARGO", align: "LEFT", bold: true},
+            { text: "TIPO PAGO: ANTICIPADO", align: "RIGHT" }
+        ]);
+
+        printer.setTextNormal();
+        printer.tableCustom([                                       // Prints table with custom settings (text, align, width, cols, bold)
+            { text: "Cajero: isra", align: "LEFT", bold: true},
+            { text: "FORMA PAGO: EFECTIVO", align: "RIGHT" }
+        ]);
+
+        printer.drawLine();
+
+        // printer.table(['Zero',"One", "Two", "Three", 'Four', 'Five']);  
+        printer.tableCustom([
+            { text: "Cant.", align: "LEFT"},
+            { text: "", align: 'LEFT'},
+            { text: "Descripción", align: "CENTER", bold: true },
+            { text: "Precio", align: "RIGHT" }
+        ]);
+
+        printer.newLine()
+
+        printer.tableCustom([
+            { text: "2", align: "LEFT"},
+            { text: "X", align: "LEFT", bold: true},
+            { text: "Lavado 8kg", align: "CENTER"},
+            { text: "$90", align: "RIGHT" }
+        ]);
+
+        printer.alignCenter()
+
+        printer.drawLine();
         // printer.print("Hello World");                               // Append text
-        // printer.println("Hello World");                             // Append text with new line
-        printer.println('PROBANDO LINE')
+        // printer.println("Hello World");  
+
+        printer.bold(true);                             // Append text with new line
+        printer.println('Total Pagado: $90')
+        printer.bold(false);
+        printer.println('Noventa Pesos 00/100 M.N.')
+
+        printer.alignLeft()
+
+        printer.drawLine();
+
+        printer.println('Cliente: Janito')
+        printer.println('F.Recepción: 23/12/2023 SABADO 11:19PM')
+        printer.bold(true);
+        printer.println('F.Entrega: 24/12/2023 DOMINGO 09:00AM')
+        printer.bold(false);   
+
+        printer.drawLine();
+
+        printer.println('Observaciones Generales: ')
+
+        printer.drawLine();
+
+        printer.println('PROFECO NO. REGISTRO: 4390/2013')
+        printer.println('NO. EXPEDIENTE PFC.B.E. 7/005243/20013')
+    
+        printer.tableCustom([
+            { text: "FECHA: 23/12/2023", align: "LEFT", bold: true},
+            { text: "HORA: 11:19PM", align: "RIGHT"},
+        ]);
+
         // printer.bold(true);                                         // Set text bold
         // printer.invert(true);                                       // Background/text color inversion
         // printer.underline(true);                                    // Underline text (1 dot thickness)
-        // printer.underlineThick(true);                               // Underline text with thick line (2 dot thickness)
-        printer.drawLine();                                         // Draws a line
+        // printer.underlineThick(true);                               // Underline text with thick line (2 dot thickness)                                        // Draws a line
         // printer.newLine();                                          // Inserts break line 
 
         // printer.alignCenter();                                      // Align text to center
@@ -76,7 +148,7 @@ export const generateTicket = async () => {
         printer.cut();                                              // Cuts the paper (if printer only supports one mode use this)
         execute()
 
-        console.log("Print done!");
+
     } catch (error) {
         console.error("Print failed:", error);
     }

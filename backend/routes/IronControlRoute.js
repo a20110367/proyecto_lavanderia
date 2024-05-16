a1495900ec77d7599497581136418ace1ad469d3
Saul <hebrobine@gmail.com>
2024-05-15 22:13:15 -0600

    sort of routes of iron Control

--- backend/routes/IronControlRoute.js
+++ backend/routes/IronControlRoute.js
@@ -24,12 +24,14 @@ router.get('/lastIronControl', getLastIronControl);
 router.get('/newIronControl', createIronControl);
 
 router.patch('/ironControl/:id', updateIronControl);
-router.patch('/updateIronRegularOrderDone/:id', updateIronRegularOrderDone);//Este es el que registra los pedidos regulares ya hechos
 
 router.patch('/updateIronRegularOrderNew/:id', updateIronRegularOrderNew);
 router.patch('/updateIronRegularOrderForTomorrow/:id', updateIronRegularOrderForTomorrow);
 router.patch('/expressNewOrderIronControl/:id', updateIronExpressOrderNew);
+
+router.patch('/updateIronRegularOrderDone/:id', updateIronRegularOrderDone);//Este es el que registra los pedidos regulares ya hechos
 router.patch('/expressDoneOrderIronControl/:id', updateIronExpressOrderDone);
+
 router.patch('/cleanCashCutIronControl/:id', updateCleanCashCutIron);
 
 

47972c68cadef47a7d312ac277334744b4326787
Saul <hebrobine@gmail.com>
2024-11-08 15:25:47 -0600

    only map works with join

--- backend/controllers/TicketController.js
+++ backend/controllers/TicketController.js
@@ -668,20 +668,20 @@ export const reprintOrder = async (req, res) => {
 
         printer.newLine()
 
-        order.ServiceOrderDetail.forEach(detail => {
+        order.ServiceOrderDetail.map(detail => {
             printer.tableCustom([
                 { text: detail.units + '     X', align: "LEFT", bold: true },
-                { text: order.fk_categoryId === 1 ? detail.SelfService.description :
-                    order.fk_categoryId === 2 ? detail.LaundryService.description :
-                    order.fk_categoryId === 3 ? detail.IronService.description :
-                    order.fk_categoryId === 4 ? detail.DrycleanService.description :
-                    order.fk_categoryId === 5 ? detail.OtherService.description :
+                { text: order.fk_categoryId === 1 ? ServiceOrderDetail.SelfService.description :
+                    order.fk_categoryId === 2 ? ServiceOrderDetail.LaundryService.description :
+                    order.fk_categoryId === 3 ? ServiceOrderDetail.IronService.description :
+                    order.fk_categoryId === 4 ? ServiceOrderDetail.DrycleanService.description :
+                    order.fk_categoryId === 5 ? ServiceOrderDetail.OtherService.description :
                     "Servicio de Categoria no Encontrada", align: "CENTER" },
-                { text: '$' + order.fk_categoryId === 1 ? detail.SelfService.price :
-                    order.fk_categoryId === 2 ? detail.LaundryService.price :
-                    order.fk_categoryId === 3 ? detail.IronService.price :
-                    order.fk_categoryId === 4 ? detail.DrycleanService.price :
-                    order.fk_categoryId === 5 ? detail.OtherService.price :
+                { text: '$' + order.fk_categoryId === 1 ? ServiceOrderDetail.SelfService.price :
+                    order.fk_categoryId === 2 ? ServiceOrderDetail.LaundryService.price :
+                    order.fk_categoryId === 3 ? ServiceOrderDetail.IronService.price :
+                    order.fk_categoryId === 4 ? ServiceOrderDetail.DrycleanService.price :
+                    order.fk_categoryId === 5 ? ServiceOrderDetail.OtherService.price :
                     "Precio de Categoria no Encontrada", align: 'RIGHT' },
                 { text: '$' + detail.subtotal, align: "RIGHT" }
             ]);

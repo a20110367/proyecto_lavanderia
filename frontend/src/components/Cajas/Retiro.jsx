baf260892f5725b521696dc08e4cab8657f59544
Saul <hebrobine@gmail.com>
2025-01-07 21:38:55 -0600

    print receipt commented by rafa petition

--- frontend/src/components/Cajas/Retiro.jsx
+++ frontend/src/components/Cajas/Retiro.jsx
@@ -134,9 +134,9 @@ function Retiro() {
         logEntry: `WARNING Retiro.jsx : ${cookies.username} has made a cashWithdrawal of $${monto} with an id: ${res.data.id_cashWithdrawal}`
       });
 
-      await api.post('/generateCashWithdrawalTicket', {
-        cashWithdrawal: cashWithdrawal,
-      })
+      // await api.post('/generateCashWithdrawalTicket', {
+      //   cashWithdrawal: cashWithdrawal,
+      // })
 
       const nuevoRetiro = {
         id_cashWithdrawal: res.data.id_cashWithdrawal,

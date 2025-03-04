e81db2bf7ed10f76bc9352b297f2b88c7fd24f08
Saul <hebrobine@gmail.com>
2025-03-03 21:31:15 -0600

    index >> id_service

--- frontend/src/components/Service/ServicesTintoreria.jsx
+++ frontend/src/components/Service/ServicesTintoreria.jsx
@@ -108,7 +108,7 @@ function ServicesTintoreria() {
                 )
                 .map((service, index) => (
                   <tr key={service.id_service}>
-                    <td>{index + 1}</td>
+                    <td>{service.id_service}</td>
                     <td>{service.description}</td>
                     <td>{service.pieces}</td>
                     <td>{service.Category.categoryDescription}</td>

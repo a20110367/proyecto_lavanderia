f48fb1f19d65534a211ddb290bfb03cda0eca6f0
Saul <hebrobine@gmail.com>
2025-06-23 14:24:20 -0600

    Notification sended only via Whatsapp Message

--- backend/controllers/MessageController.js
+++ backend/controllers/MessageController.js
@@ -25,14 +25,14 @@ export const sendMessage = async (req, res) => {
     `
 
     try {
-        const info = await transporter.sendMail({
-            from: `"${subject}👻" <pyrop59@gmail.com>`, // sender address
-            to: email, // list of receivers
-            subject: subject, // Subject line
-            text: text, // plain text body
-            // html: "<b>Tamal</b>", // html body
-            html: output,
-        });
+        // const info = await transporter.sendMail({
+        //     from: `"${subject}👻" <pyrop59@gmail.com>`, // sender address
+        //     to: email, // list of receivers
+        //     subject: subject, // Subject line
+        //     text: text, // plain text body
+        //     // html: "<b>Tamal</b>", // html body
+        //     html: output,
+        // });
 
         console.log("Mail Message sent:  %s", info.messageId);
 
@@ -50,35 +50,35 @@ export const notifyAll = async (req, res) => {
     for (const order of filteredOrder) {
         console.log(order.id_order)
         const message = `Tu pedido con el folio: ${order.id_order} está listo, Ya puedes pasar a recogerlo.`
-        const subject = `Tu Ropa esta Lista ${order.client.name}`
-        const text = `Tu ropa esta lista, esperamos que la recojas a su brevedad`
+    //     const subject = `Tu Ropa esta Lista ${order.client.name}`
+    //     const text = `Tu ropa esta lista, esperamos que la recojas a su brevedad`
 
-        const output = `
-        <h3>Detalles del Pedido:</h3>
-        <ul>  
-            <li>Folio: ${order.id_order}</li> 
-            <li>Nombre: ${order.client.name}</li>
-            <li>Correo Electronico: ${order.client.email}</li>            
-        </ul>
-        <h3>Cuerpo</h3>
-        <p>${message}</p>
-    `
+    //     const output = `
+    //     <h3>Detalles del Pedido:</h3>
+    //     <ul>  
+    //         <li>Folio: ${order.id_order}</li> 
+    //         <li>Nombre: ${order.client.name}</li>
+    //         <li>Correo Electronico: ${order.client.email}</li>            
+    //     </ul>
+    //     <h3>Cuerpo</h3>
+    //     <p>${message}</p>
+    // `
 
         try {
-            const info = await transporter.sendMail({
-                from: `"${subject}👻" <pyrop59@gmail.com>`, // sender address
-                to: order.client.email, // list of receivers
-                subject: subject, // Subject line
-                text: text, // plain text body
-                // html: "<b>Tamal</b>", // html body
-                html: output,
-            });
+            // const info = await transporter.sendMail({
+            //     from: `"${subject}👻" <pyrop59@gmail.com>`, // sender address
+            //     to: order.client.email, // list of receivers
+            //     subject: subject, // Subject line
+            //     text: text, // plain text body
+            //     // html: "<b>Tamal</b>", // html body
+            //     html: output,
+            // });
 
             console.log("Mail Message sent:  %s", info.messageId);
 
-            // restAPI.message.sendMessage(tel+"@c.us", null , message).then((data) => {
-            //     console.log("Whatsapp Message sent:  %s", data);
-            // });    
+            restAPI.message.sendMessage(tel+"@c.us", null , message).then((data) => {
+                console.log("Whatsapp Message sent:  %s", data);
+            });    
         } catch (err) {
             console.log(err)
             return res.status(400).json({ message: 'Algo salio mal!' })
